661e9b5e4677f775fe52103c05eec5c48a828c08
Ali-dev <alin89328@gmail.com>
2023-05-24 18:18:20 +0500

    feat: Added get recent posts graphQL query

--- services/index.js
+++ services/index.js
@@ -38,3 +38,24 @@ export const getPosts = async () => {
   return results.postsConnection.edges
     
 }
+
+export const getRecentPosts = async () => {
+  const query = gql`
+    query GetPostDetails() {
+      posts(
+        orderBy: createdAt_ASC
+        last: 3
+      ) {
+        title
+        featuredImage {
+          url
+        }
+        createdAt
+        slug
+      }
+    }
+  `;
+  const result = await request(graphqlAPI, query);
+
+  return result.posts;
+};
