33909cde9636d010478d7e01bfa70a37d8b35361
Ali-dev <alin89328@gmail.com>
2023-05-24 16:07:40 +0500

    feat: Created a basic code setup for blog app

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,15 @@
-import '@/styles/globals.css'
+import React, { useEffect, useState } from 'react'
+
+import { Layout } from '@/components'
+
+import 'tailwindcss/tailwind.css'
+import '@/styles/globals.scss'
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <Layout>
+      <Component {...pageProps} />
+    </Layout>
+
+  )
 }
