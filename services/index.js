22b82a2aa0f03c64352d4d2944a86784ae0605d6
Ali-dev <alin89328@gmail.com>
2023-05-25 14:46:17 +0500

    feat: GraphQL query to get similar posts

--- services/index.js
+++ services/index.js
@@ -59,3 +59,24 @@ export const getRecentPosts = async () => {
 
   return result.posts;
 };
+
+export const getSimilarPosts = async (categories, slug) => {
+  const query = gql`
+    query GetPostDetails($slug: String!, $categories: [String!]) {
+      posts(
+        where: {slug_not: $slug, AND: {categories_some: {slug_in: $categories}}}
+        last: 3
+      ) {
+        title
+        featuredImage {
+          url
+        }
+        createdAt
+        slug
+      }
+    }
+  `;
+  const result = await request(graphqlAPI, query, { slug, categories });
+
+  return result.posts;
+};
