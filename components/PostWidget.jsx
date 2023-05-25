6302d93777f62435ba9132e950dbd7c3352ea006
Ali-dev <alin89328@gmail.com>
2023-05-25 15:42:13 +0500

    feat: Created post categories component

--- components/PostWidget.jsx
+++ components/PostWidget.jsx
@@ -2,7 +2,7 @@ import React, { useState, useEffect } from 'react';
 import moment from 'moment';
 import Link from 'next/link';
 
-import { getSimilarPosts, getRecentPosts } from '../services';
+import { getSimilarPosts, getRecentPosts } from '@/services';
 
 const PostWidget = ({ categories, slug }) => {
   const [relatedPosts, setRelatedPosts] = useState([]);
